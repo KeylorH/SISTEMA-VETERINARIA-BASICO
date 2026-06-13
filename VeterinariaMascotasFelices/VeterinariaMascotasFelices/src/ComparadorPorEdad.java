2957512a252124ea0801ffb261a72b0d074366f4
Keylor Herrera Fuentes <84596023+KeylorH@users.noreply.github.com>
2026-06-12 23:50:58 -0600

    Delete VeterinariaMascotasFelices/VeterinariaMascotasFelices/src directory

--- VeterinariaMascotasFelices/VeterinariaMascotasFelices/src/ComparadorPorEdad.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorPorEdad implements Comparator<Animal> {
-    @Override
-    public int compare(Animal a1, Animal a2) {
-        return Integer.compare(a1.getEdad(), a2.getEdad());
-    }
-}
--- VeterinariaMascotasFelices/VeterinariaMascotasFelices/src/ComparadorPorPeso.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorPorPeso implements Comparator<Animal> {
-    @Override
-    public int compare(Animal a1, Animal a2) {
-        return Double.compare(a1.getPeso(), a2.getPeso());
-    }
-}
